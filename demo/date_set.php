a89c2331deec48d7711c0b0224215874dab24a03
Tej2709 <100223625+Tej2709@users.noreply.github.com>
2022-03-26 18:55:51 +0530

    Add files via upload
    Date Functions

---
+++ demo/date_set.php
@@ -0,0 +1,8 @@
+<?Php
+
+    $date=date_create();
+    date_date_set($date,2022,03,26);
+    echo date_format($date, "D/M/Y");
+
+
+?>
---
+++ demo/date_str.php
@@ -0,0 +1,6 @@
+<?php
+echo(strtotime("now") . "<br>");
+echo(strtotime("5 April 2022") . "<br>");
+echo(strtotime("+5 days") . "<br>");
+
+?>
